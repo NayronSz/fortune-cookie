a64ba79802a0ed71a22b866ce2ae854da2c4e4c2
Nayron Souza <100665374+NayronSz@users.noreply.github.com>
2023-04-01 10:28:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,31 +4,30 @@ const imgCookieOpened = document.querySelector("#fortune-cookie-img");
 const firstScreen = document.querySelector(".first-screen");
 const secondScreen = document.querySelector(".second-screen");
 const textFortune = document.querySelector("#text-fortune");
-
+let index = 0;
 const textList = [
   "Não se desespere quando a caminhada estiver difícil, é sinal de que você está no caminho certo.",
   "Não viva para que a sua presença seja notada, mas para que a sua falta seja sentida.",
 ];
 
-let index = 0;
-
 const tryAnotherCookie = btnCookie.addEventListener("click", notReload);
+const cookieTest = imgCookieOpened.addEventListener("click", toogleScreens);
+const nwCookie = btnNewCookie.addEventListener("click", toogleScreens);
+const textToogle = imgCookieOpened.addEventListener("click", textValidation);
+
 function notReload(e) {
   e.preventDefault();
 }
-
-const cookieTest = imgCookieOpened.addEventListener("click", toogleScreens);
-const nwCookie = btnNewCookie.addEventListener("click", toogleScreens);
 
 function toogleScreens() {
   firstScreen.classList.toggle("hide");
   secondScreen.classList.toggle("hide");
 }
 
-imgCookieOpened.addEventListener("click", function () {
-  textFortune.innerText = textList[index];
+function textValidation() {
+  const currentText = (textFortune.innerText = textList[index]);
   index++;
   if (index >= textList.length) {
     index = 0;
   }
-});
+}
